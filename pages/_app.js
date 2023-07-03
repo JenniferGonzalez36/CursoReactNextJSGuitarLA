22929bcf3b2a4f88c220c710d7511a66baf5a84b
Jennifer González <jenniferjacqueline936@gmail.com>
2023-07-03 01:10:08 -0600

    Implementing LocalStorage

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import '../styles/globals.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
 
-  const [carrito, setCarrito] = useState([]);
+  const carritoLS = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('carrito')) ?? [] : [];
+  const [carrito, setCarrito] = useState(carritoLS);
+
+  const [paginaLista, setPaginaLista] = useState(false);
+
+  useEffect(() => {
+    setPaginaLista(true);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('carrito',  JSON.stringify(carrito));
+  }, [carrito])
 
   const agregarCarrito = guitarra => {
     // Comprobar si la guitarra ya esta en el carrito...
@@ -42,12 +53,12 @@ function MyApp({ Component, pageProps }) {
     window.localStorage.setItem('carrito', JSON.stringify( carrito ));
   }
 
-  return <Component {...pageProps} 
+  return paginaLista ? <Component {...pageProps} 
     carrito = { carrito }
     agregarCarrito = { agregarCarrito }
     eliminarProducto = { eliminarProducto }
     actualizarCantidad = { actualizarCantidad }
-  />
+  /> : null;
 }
 
 export default MyApp
